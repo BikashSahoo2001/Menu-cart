79ba902ab466af9824ec54b88aceae46ea0b5444
Bikash sahoo <133564845+BikashSahoo2001@users.noreply.github.com>
2023-09-21 14:55:55 +0530

    Add files via upload

---
+++ restropress-menu-cart/includes/class-restropress-menu-cart-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://author.example.com/
+ * @since      1.0.0
+ *
+ * @package    Restropress_Menu_Cart
+ * @subpackage Restropress_Menu_Cart/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Restropress_Menu_Cart
+ * @subpackage Restropress_Menu_Cart/includes
+ * @author     Bikash Sahoo <bikashsahoobiki1999@gmail.com>
+ */
+class Restropress_Menu_Cart_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
